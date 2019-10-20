51ba5fd9a4d9d757b9e6b943cdec1bc60d07d9b4
Christophe Moreira <moreira.christophe@outlook.com>
2019-10-20 21:16:17 +0200

    added snackbar materialui

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,20 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Slider from "rc-slider";
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 const Navbar = ({ level, setLevel, format, setFormat }) => {
-  
+  const [open, setOpen] = useState(true);
+  const handleFormatChange = (e) =>{
+    setFormat(e.target.value);
+    setOpen(true)
+  }
   return (
     <div>
       <header className="Navbar">
@@ -30,15 +37,34 @@ const Navbar = ({ level, setLevel, format, setFormat }) => {
             />
           </div>
         </div>
-        <div className='select-container'>
-          <Select value={format} onChange={e=>{
-            setFormat(e.target.value)
-          }}>
+        <div className="select-container">
+          <Select
+            value={format}
+            onChange={handleFormatChange}
+          >
             <MenuItem value="hex">HEX - #fffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
             <MenuItem value="rgba">RGBa - rgb(255,255,255,1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "left" }}
+          open={open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format Changed To {format.toUpperCase()}</span>}
+          ContentProps={{ "aria-describedby": "message-id" }}
+          onClose={() => setOpen(false)}
+          action={[
+            <IconButton
+              onClick={() => setOpen(false)}
+              color="inherit"
+              key='close'
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     </div>
   );
